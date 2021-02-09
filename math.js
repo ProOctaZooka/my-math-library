146fab3eced97ba129517a4641938b878cc4bff6
Thanet Kittisuntrakul <thanet.kit@dome.tu.ac.th>
2021-02-09 11:48:01 +0700

    Add sum array functionality

---
+++ math.js
@@ -0,0 +1,14 @@
+function sum(a, b){
+    if(Array.isArray(a)){
+        let total = 0
+        for(let i = 0; i < a.length; i++){
+            total += a[i]
+        }
+        return total
+    }
+return a + b
+}
+
+module.exports = {
+    sum
+}
